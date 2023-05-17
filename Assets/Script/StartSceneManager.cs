86530ac3cfa81f1fc8baa2bbfe332bfde07d8398
zufall0067 <68945314+zufall0067@users.noreply.github.com>
2023-05-17 10:45:05 +0900

    a
    a

--- Assets/Script/StartSceneManager.cs
+++ Assets/Script/StartSceneManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class StartSceneManager : MonoBehaviour
 {
+    public GameObject helpWindow;
+
     public void GameStart()
     {
         SceneManager.LoadScene("SampleScene");
@@ -12,6 +14,34 @@ public class StartSceneManager : MonoBehaviour
 
     public void GameQuit()
     {
-        Application.Quit();
+        Debug.Log("Ending");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+    }
+
+    public void HelpWindowOpen()
+    {
+        if (!helpWindow.activeSelf)
+            helpWindow.gameObject.SetActive(true);
+    }
+
+    public void HelpWindowClose()
+    {
+        if(helpWindow.activeSelf) 
+        {
+            helpWindow.gameObject.SetActive(false);
+        }
+        
+    }
+
+    public void esterEgg()
+    {
+        if(Random.Range(0, 100) < 10)
+        {
+            SceneManager.LoadScene("StartScene");
+        }
     }
 }
